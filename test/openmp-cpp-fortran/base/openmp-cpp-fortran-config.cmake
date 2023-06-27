5670e5e08bf3d91a3632a39657e1230309242e52
David Beckingsale <davidbeckingsale@gmail.com>
2023-06-27 14:24:14 -0700

    Add test for openmp cpp fortran issue

---
+++ test/openmp-cpp-fortran/base/openmp-cpp-fortran-config.cmake
@@ -0,0 +1,1 @@
+include("${CMAKE_CURRENT_LIST_DIR}/openmp-cpp-fortran-targets.cmake")
---
+++ test/openmp-cpp-fortran/base/openmp-cpp-fortran.cpp
@@ -0,0 +1,19 @@
+#include <omp.h>
+#include <iostream>
+
+void test_func()
+{
+
+  #pragma omp parallel
+  {
+    int thId = omp_get_thread_num();
+    int thNum = omp_get_num_threads();
+    int thMax = omp_get_max_threads();
+
+    #pragma omp critical
+    std::cout <<"\nMy thread id is: " << thId
+              <<"\nNum threads is: " << thNum
+              <<"\nMax threads is: " << thMax
+              << std::endl;
+  }
+}
